86cda3d3bfb65dfed9afbcc88a8272219c31b894
Krishna <krishnamewara841@gmail.com>
2025-01-26 17:45:58 +0530

    initial pages implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:travel_app/Pages/homepage/home_page.dart';
+import 'package:travel_app/Pages/forgotpasswordpage/forgotpassword.dart';
+import 'package:travel_app/Pages/loginpage/loginpage.dart';
+import 'package:travel_app/Pages/signuppage/signuppage.dart';
+import 'package:travel_app/Pages/otpverificationpage/otpverification.dart';
+import 'package:travel_app/Pages/welcomepage/welcomepage.dart';
+import 'package:travel_app/Pages/splashscreen/splashscreen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -12,7 +18,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(), // Set your custom HomePage here
+      initialRoute: '/splash',  // Set the splash screen as initial route
+      routes: {
+        '/splash': (context) => SplashScreen(),  // Add splash screen route
+        '/welcome': (context) => WelcomePage(),
+        '/login': (context) => LoginPage(),
+        '/signup': (context) => SignupPage(),
+        '/forgotPassword': (context) => ForgotPasswordPage(),
+        '/otpVerification': (context) => OtpverificationPage(),
+      },
+      debugShowCheckedModeBanner: false, // Disable debug banner
     );
   }
 }
